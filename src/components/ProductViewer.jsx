660392446ab91b07fc54f6acc72f63d79f5cb067
Subransu <maharanasubhransu6@gmail.com>
2025-10-11 20:31:15 +0530

    Adding studio lights to the model

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -4,6 +4,7 @@ import {clsx} from "clsx";
 import {Canvas} from "@react-three/fiber";
 import {Box, OrbitControls} from "@react-three/drei";
 import MacBookModel14 from "./models/Macbook-14.jsx";
+import StudioLights from "./StudioLights.jsx";
 
 const ProductViewer = () => {
     const {color, scale, setColor, setScale} = useMacBookStore();
@@ -32,7 +33,7 @@ const ProductViewer = () => {
             </div>
 
             <Canvas id='canvas' camera={{position: [0, 2, 5], fov: 50, near: 0.1, far: 100}} >
-                <ambientLight intensity={0.5}/>
+                <StudioLights/>
                 <MacBookModel14 scale={0.06} position={[0, 0, 0]}/>
                 <OrbitControls enableZoom={false}/>
             </Canvas>
