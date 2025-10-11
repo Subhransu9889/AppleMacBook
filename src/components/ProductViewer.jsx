2970ec3f39c445ec48e7cb96350a1225ed9d52db
Subransu <maharanasubhransu6@gmail.com>
2025-10-11 20:17:28 +0530

    Implementing the macbook model glb to jsx and render it

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -3,6 +3,7 @@ import useMacBookStore from "../store/index.js";
 import {clsx} from "clsx";
 import {Canvas} from "@react-three/fiber";
 import {Box, OrbitControls} from "@react-three/drei";
+import MacBookModel14 from "./models/Macbook-14.jsx";
 
 const ProductViewer = () => {
     const {color, scale, setColor, setScale} = useMacBookStore();
@@ -31,7 +32,8 @@ const ProductViewer = () => {
             </div>
 
             <Canvas id='canvas' camera={{position: [0, 2, 5], fov: 50, near: 0.1, far: 100}} >
-                <Box position={[0, 0, 0]} scale={10*scale} material-color={color}></Box>
+                <ambientLight intensity={0.5}/>
+                <MacBookModel14 scale={0.06} position={[0, 0, 0]}/>
                 <OrbitControls enableZoom={false}/>
             </Canvas>
         </section>
