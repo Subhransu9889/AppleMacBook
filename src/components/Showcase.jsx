540ac29f924d7985b98483b02f9baaa7dc2111c6
Subransu <maharanasubhransu6@gmail.com>
2025-10-12 20:38:45 +0530

    Updating the Showcase section

--- src/components/Showcase.jsx
+++ src/components/Showcase.jsx
@@ -1,53 +1,50 @@
-import React from 'react'
 import {useMediaQuery} from "react-responsive";
 import {useGSAP} from "@gsap/react";
-import gsap from "gsap";
+import gsap from 'gsap';
 
 const Showcase = () => {
-    const isTablet = useMediaQuery({query: '(max-width: 1024px)'});
+    const isTablet = useMediaQuery({ query: '(max-width: 1024px)'});
 
     useGSAP(() => {
         if(!isTablet) {
             const timeline = gsap.timeline({
                 scrollTrigger: {
                     trigger: '#showcase',
-                     start: 'top top',
-                     end: 'bottom top',
-                     scrub: true,
-                     pin: true,
+                    start: 'top top',
+                    end: 'bottom top',
+                    scrub: true,
+                    pin: true,
                 }
             });
 
-            timeline.to('.mask img', {
-                transform: 'scale(1.1)'
-            }).to('.content', {
-                opacity: 1,
-                y: 0,
-                ease: 'power1.in'
-            })
+            timeline
+                .to('.mask img', {
+                    transform: 'scale(1.1)'
+                }).to('.content', { opacity: 1, y: 0, ease: 'power1.in' });
         }
-    }, [])
+    }, [isTablet])
 
     return (
-        <section id='showcase'>
-            <div className='media'>
-                <video src='/videos/game.mp4' loop autoPlay muted playsInline/>
-                <div className='mask'>
-                    <img src='/mask-logo.svg' alt='M4logo'/>
+        <section id="showcase">
+            <div className="media">
+                <video src="/videos/game.mp4" loop muted autoPlay playsInline />
+                <div className="mask">
+                    <img src="/mask-logo.svg" />
                 </div>
             </div>
 
-            <div className='content'>
-                <div className='wrapper'>
-                    <div className='lg:max-w-md'>
+            <div className="content">
+                <div className="wrapper">
+                    <div className="lg:max-w-md">
                         <h2>Rocket Chip</h2>
 
-                        <div className='space-y-5 mt-7 pe-10'>
-                            <p>Introducing {" "}
-                                <span className='text-white'>
+                        <div className="space-y-5 mt-7 pe-10">
+                            <p>
+                                Introducing {" "}
+                                <span className="text-white">
                                     M4, the next generation of Apple silicon
                                 </span>
-                                .M4 powers
+                                . M4 powers
                             </p>
                             <p>
                                 It drives Apple Intelligence on iPad Pro, so you can write, create, and accomplish more with ease. All in a design that’s unbelievably thin, light, and powerful.
